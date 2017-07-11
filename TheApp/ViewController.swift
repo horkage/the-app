1e39e93426fee5ac454e7d32edb483f798a06eca
Michael Wood <mwood@Michaels-iMac.local>
2017-07-11 11:21:51 -0700

    Initial Commit

---
+++ TheApp/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  TheApp
+//
+//  Created by Michael Wood on 7/11/17.
+//  Copyright © 2017 Michael Wood. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
